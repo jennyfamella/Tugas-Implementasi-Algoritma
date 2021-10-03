8079c3f93725c4e87fc8ec47f5574aaebc427cc7
jennyfamella <jennyfamella@student.telkomuniversity.ac.id>
2021-10-03 18:55:38 +0700

    HelloWord.java

---
+++ DemoGit/HelloWord.java
@@ -0,0 +1,10 @@
+public class HelloWord{
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello Word");
+		System.out.println("Nama : Jenny Famella Putri");
+		System.out.println("NIM  : 6706213018");
+		System.out.println("Kelas: RPLA 45-02");
+	}
+}
